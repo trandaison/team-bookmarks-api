688fbab80f500c78f9e402e4d07e375429542b12
Tran Dai Son <sontd@nal.vn>
2022-02-21 20:51:09 +0700

    Update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::API
+  wrap_parameters false
   include Pagy::Backend
+
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    render json: { error: exception.message }, status: :not_found
+  end
+
+  rescue_from ActionController::ParameterMissing do |exception|
+    render json: { error: exception.message }, status: :bad_request
+  end
 
   private
   def pagy_meta(pagy)
